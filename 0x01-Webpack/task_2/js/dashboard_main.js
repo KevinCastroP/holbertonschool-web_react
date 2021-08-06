0a1b5ec5b757518475e2fcefcb5f32b5178cb4cf
KevinCastroP <castrok.c11@hotmail.com>
2021-08-06 12:10:47 -0500

    webpack

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,6 +1,6 @@
 const $ = require("jquery");
 const _ = require("lodash");
-import "./body.css";
+import "../css/main.css";
 
 let count = 0;
 
@@ -17,6 +17,9 @@ const $button = $("<button>Click here to get started</button>").on(
   })
 );
 
+$("body").append("<div id='logo'></div>");
+$("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
 $("body").append($button);
 $("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
